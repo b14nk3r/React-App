4b82cb363ba8fc52b5f9154d773b9e9e8c73bc07
b14nk3r <b1anker.kr@gmail.com>
2023-01-19 03:03:05 +0900

    테스트

--- react-app/src/Reference.jsx
+++ react-app/src/Reference.jsx
@@ -11,10 +11,31 @@ import ReactHtmlParser from 'react-html-parser'
 import Axios from 'axios'; 
 
 function Reference() {
-    const [referenceContent, setReferenceContent] = useState({
-        title: '',
-        content: ''
-    })
+    const [name, setName] = useState('');
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [file, setFile] = useState()
+
+    const onChangeName = (e) => {
+        setName({
+            ...name,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onChangeTitle = (e) => {
+        setTitle({
+            ...title,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onChangeContent = (e) => {
+        setContent({
+            ...content,
+            [e.target.name]: e.target.value
+        });
+    }
 
     const [viewContent, setViewContent] = useState([]);
 
@@ -25,25 +46,25 @@ function Reference() {
       },[viewContent])
     //이것이주석이야sdasdasdasdasd
 
-    const submitReview = ()=>{
-        Axios.post('http://localhost:8080/add', {
-          title: referenceContent.title,
-          content: referenceContent.content
-          
-        }).then(()=>{
-          alert('등록 완료!');
-          console.log(referenceContent.title);
-        })
-      };
+    function onSubmit(e){
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("file", et.target.files[0]);
+        formData.append("name", name);
+        formData.append("title", title);
+        formData.append("content", content);
 
-    const getValue = e => {
-        const { name, value } = e.target;
-        setReferenceContent({
-            ...referenceContent,
-            [name]: value
-        })
-        console.log(name, value);
+        Axios({
+            headers: {
+              "Content-Type": "multipart/form-data",
+              "Access-Control-Allow-Origin": "*",
+            },
+            url: "ttp://localhost:8080/add", // 파일 업로드 요청 URL
+            method: "POST",
+            data: formData,
+          });
     }
+
     return (
         <Container>
             <div class="form-group">
@@ -59,63 +80,42 @@ function Reference() {
                         </div>)}
                 </div>
             </div>
-            <div>
-                <div class="form-group">
-                    <label for="exampleInputContent1" class="form-label mt-4">제목</label>
-                    <input class="form-control" type="text"
-                        placeholder="제목을 입력하세요."
-                        onChange={getValue}
-                        name='title'
-                    />
+            <form onSubmit={onSubmit} enctype="multipart/form-data" >
+                <div>
+                    <div class="form-group">
+                        <label for="exampleInputContent1" class="form-label mt-4">제목</label>
+                        <input class="form-control" type="text"
+                            placeholder="제목을 입력하세요."
+                            onChange={onChangeTitle}
+                            name='title'
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputWriter1" class="form-label mt-4">이름</label>
+                        <input class="form-control" type="text"
+                            placeholder="이름을 입력하세요."
+                            onChange={onChangeName}
+                            name='name'
+                        />
+                    </div>
+                    <div>
+                        <input class="form-control" type="file" name="file" />
+                    </div>
+                    <div class="form-group">
+
+                        <label for="exampleInputContent1" class="form-label mt-4">내용</label>
+                        <input class="form-control" type="text"
+                            placeholder="내용을 입력하세요."
+                            onChange={onChangeContent}
+                            name='content'
+                        />
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputWriter1" class="form-label mt-4">이름</label>
-                    <input class="form-control" type="text"
-                        placeholder="이름을 입력하세요."
-                        onChange={getValue}
-                        name='title'
-                    />
-                </div>          
-                <div class="form-group">
-                    <label for="exampleInputPassword1" class="form-label mt-4">비밀번호</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="비밀번호를 입력하세요." />
-                </div>
-                <form method="POST" action="/upload" enctype="multipart/form-data" >
-                    <input type="file" name="file" />
-                    <button type="submit">전송</button>
-                </form>
-            {/* bootstrap 형식으로 바꿔라 씹새야 */}
-                <div class="form-group">
-                
-                    <label for="exampleInputContent1" class="form-label mt-4">내용</label>
-                    <CKEditor class="form-control"
-                        editor={ClassicEditor}
-                        data="<p>Hello from CKEditor 5!</p>" 
-                        onReady={editor => {
-                            // You can store the "editor" and use when it is needed.
-                            console.log('Editor is ready to use!', editor);
-                        }}
-                        onChange={(event, editor) => {
-                            const data = editor.getData();
-                            console.log({ event, editor, data });
-                            setReferenceContent({
-                                ...referenceContent,
-                                content: data
-                            })
-                        }}
-                        onBlur={(event, editor) => {
-                            console.log('Blur.', editor);
-                        }}
-                        onFocus={(event, editor) => {
-                            console.log('Focus.', editor);
-                        }}
-                    />
-                </div>
-            </div>
-            <Button 
-            className="submit-button"
-            onClick={submitReview}
-            >입력</Button>
+                <Button
+                    className="submit-button"
+                    type="submit"
+                >입력</Button>
+            </form>
         </Container>
     )
 }
