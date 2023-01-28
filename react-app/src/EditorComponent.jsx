5959a91e2e406a4f72936108bd1781a674ccc54c
b14nk3r <b1anker.kr@gmail.com>
2023-01-29 02:48:39 +0900

    React Quill 적용 및 Taylor의 백엔드 업데이트

---
+++ react-app/src/EditorComponent.jsx
@@ -0,0 +1,104 @@
+import { useRef, useState, useMemo } from "react";
+
+//이렇게 라이브러리를 불러와서 사용하면 됩니다
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+import Axios from 'axios';
+
+
+const EditorComponent = ({content, setContent}) => {
+
+
+    const imageHandler = () => {
+        console.log('에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!');
+
+        // 1. 이미지를 저장할 input type=file DOM을 만든다.
+        const input = document.createElement('input');
+        // 속성 써주기
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+        input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
+        // input이 클릭되면 파일 선택창이 나타난다.
+
+        // input에 변화가 생긴다면 = 이미지를 선택
+        input.addEventListener('change', async () => {
+            console.log('온체인지');
+            const file = input.files[0];
+            // multer에 맞는 형식으로 데이터 만들어준다.
+            const formData = new FormData();
+            formData.append('img', file); // formData는 키-밸류 구조
+            // 백엔드 multer라우터에 이미지를 보낸다.
+
+            try {
+                const result = await Axios.post('http://localhost:8080/img', formData);
+                console.log('성공 시, 백엔드가 보내주는 데이터', result.data.url);
+                const IMG_URL = result.data.url;
+                // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+                // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+                // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+
+                // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
+                const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
+                // 1. 에디터 root의 innerHTML을 수정해주기
+                // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
+                // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
+                // editor.root.innerHTML =
+                //   editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
+
+                // 2. 현재 에디터 커서 위치값을 가져온다
+                const range = editor.getSelection();
+                // 가져온 위치에 이미지를 삽입한다
+                editor.insertEmbed(range.index, 'image', IMG_URL);
+            } catch (error) {
+                console.log('실패했어요ㅠ');
+            }
+        });
+    };
+    // useMemo를 사용하지 않으면, 키를 입력할 때마다, imageHandler 때문에 focus가 계속 풀리게 됩니다.
+    const modules = useMemo(() => {
+        return {
+            toolbar: {
+                container: [
+                    [{ header: [1, 2, 3, false] }],
+                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+                    ['link', 'image'],
+                    [{ 'align': [] }, { 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+                    ['clean']
+
+                ],
+                handlers: {
+                    // 이미지 처리는 우리가 직접 imageHandler라는 함수로 처리할 것이다.
+                    image: imageHandler,
+                },
+            },
+        };
+    }, []);
+    // 위에서 설정한 모듈들 foramts을 설정한다
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image',
+        'align', 'color', 'background',        
+      ];
+    //const [value, onChange] = useState(''); // 에디터 속 콘텐츠를 저장하는 state
+    const quillRef = useRef(); // 에디터 접근을 위한 ref return (
+
+    return (
+        <>
+            <ReactQuill
+                ref={quillRef}
+                theme="snow"
+                placeholder="플레이스 홀더"
+                value={content}
+                onChange={setContent}
+                modules={modules}
+                formats={formats}
+            />
+        </>
+    )
+}
+
+export default EditorComponent
--- react-app/src/Reference.jsx
+++ react-app/src/Reference.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
 
@@ -10,33 +10,30 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import ReactHtmlParser from 'react-html-parser'
 import Axios from 'axios';
 
-var 앙기모디;
+import EditorComponent from "./EditorComponent"
+//import ReactQuill
+import ReactQuill from "react-quill";
 
 function Reference() {
+    
     const [name, setName] = useState('');
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [file, setFile] = useState()
 
     const onChangeName = (e) => {
-        setName({
-            ...name,
-            [e.target.name]: e.target.value
-        });
+        setName(e.target.value);
     }
 
     const onChangeTitle = (e) => {
-        setTitle({
-            ...title,
-            [e.target.name]: e.target.value
-        });
+        setTitle(e.target.value);
     }
 
     const onChangeContent = (e) => {
-        setContent({
-            ...content,
-            [e.target.name]: e.target.value
-        });
+        setContent(e.target.value);
+    }
+    const onChangeFile = (e) => {
+        setFile(e.target.files[0]);
     }
 
     const [viewContent, setViewContent] = useState([]);
@@ -51,20 +48,30 @@ function Reference() {
     function onSubmit(e){
         e.preventDefault();
         const formData = new FormData();
-        formData.append("file", et.target.files[0]);
-        formData.append("name", name);
+        formData.append("file", file);
+        formData.append("name",name);
         formData.append("title", title);
         formData.append("content", content);
-
+        console.log(formData);
+        console.log(file);
         Axios({
-            headers: {
-              "Content-Type": "multipart/form-data",
-              "Access-Control-Allow-Origin": "*",
-            },
-            url: "ttp://localhost:8080/add", // 파일 업로드 요청 URL
             method: "POST",
+            url: 'http://localhost:8080/add', 
             data: formData,
+            headers:  {
+                "Content-Type": "multipart/form-data; charset=utf-8;",
+                "Access-Control-Allow-Origin": "*",
+              },
           });
+          
+    
+        for (let key of formData.keys()) {
+            console.log(key, ":", formData.get(key));
+        }
+        for (let value of formData.values()) {
+            console.log(value);
+      }
+        
     }
 
     return (
@@ -82,7 +89,8 @@ function Reference() {
                         </div>)}
                 </div>
             </div>
-            <form onSubmit={onSubmit} enctype="multipart/form-data" >
+            
+            <form  accept-charset="UTF-8" onSubmit={onSubmit} >
                 <div>
                     <div class="form-group">
                         <label for="exampleInputContent1" class="form-label mt-4">제목</label>
@@ -101,9 +109,11 @@ function Reference() {
                         />
                     </div>
                     <div>
-                        <input class="form-control" type="file" name="file" />
+                        <input class="form-control" onChange={onChangeFile} enctype="multipart/form-data" type="file" name="file" />
                     </div>
-                    <div class="form-group">
+                    <EditorComponent content={content} setContent={setContent} />
+
+                   {/*} <div class="form-group">
 
                         <label for="exampleInputContent1" class="form-label mt-4">내용</label>
                         <input class="form-control" type="text"
@@ -111,7 +121,7 @@ function Reference() {
                             onChange={onChangeContent}
                             name='content'
                         />
-                    </div>
+                    </div>*/}
                 </div>
                 <Button
                     className="submit-button"
@@ -123,3 +133,128 @@ function Reference() {
 }
 
 export default Reference
+/*
+import { useEffect, useState } from 'react';
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+
+//import CKeditor
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+//import ReactHtmlParser
+import ReactHtmlParser from 'react-html-parser'
+import Axios from 'axios'; 
+
+function Reference() {
+    const [referenceContent, setReferenceContent] = useState({
+        title: '',
+        content: ''
+    })
+
+    const [viewContent, setViewContent] = useState([]);
+
+    useEffect(()=>{
+        Axios.get('http://localhost:8080/list').then((response)=>{
+          setViewContent(response.data);
+        })
+      },[viewContent])
+    //이것이주석이야sdasdasdasdasd
+
+    const submitReview = ()=>{
+        Axios.post('http://localhost:8080/add', {
+          title: referenceContent.title,
+          content: referenceContent.content
+          
+        }).then(()=>{
+          alert('등록 완료!');
+          console.log(referenceContent.title);
+        })
+      };
+
+    const getValue = e => {
+        const { name, value } = e.target;
+        setReferenceContent({
+            ...referenceContent,
+            [name]: value
+        })
+        console.log(name, value);
+    }
+    return (
+        <Container>
+            <div class="form-group">
+                <h1>자료실</h1>
+                <div>
+                    {viewContent.map(element =>
+                        <div>
+                            <h2>{element.제목}</h2>
+                            <div>
+                                {ReactHtmlParser(element.내용)}
+                            </div>
+                            <img></img>
+                        </div>)}
+                </div>
+            </div>
+            <div>
+                <div class="form-group">
+                    <label for="exampleInputContent1" class="form-label mt-4">제목</label>
+                    <input class="form-control" type="text"
+                        placeholder="제목을 입력하세요."
+                        onChange={getValue}
+                        name='title'
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputWriter1" class="form-label mt-4">이름</label>
+                    <input class="form-control" type="text"
+                        placeholder="이름을 입력하세요."
+                        onChange={getValue}
+                        name='title'
+                    />
+                </div>          
+                <div class="form-group">
+                    <label for="exampleInputPassword1" class="form-label mt-4">비밀번호</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="비밀번호를 입력하세요." />
+                </div>
+                <form method="POST" action="/upload" enctype="multipart/form-data" >
+                    <input type="file" name="file" />
+                    <button type="submit">전송</button>
+                </form>
+             
+                <div class="form-group">
+                
+                    <label for="exampleInputContent1" class="form-label mt-4">내용</label>
+                    <CKEditor class="form-control"
+                        editor={ClassicEditor}
+                        data="<p>Hello from CKEditor 5!</p>" 
+                        onReady={editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log('Editor is ready to use!', editor);
+                        }}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            console.log({ event, editor, data });
+                            setReferenceContent({
+                                ...referenceContent,
+                                content: data
+                            })
+                        }}
+                        onBlur={(event, editor) => {
+                            console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            console.log('Focus.', editor);
+                        }}
+                    />
+                </div>
+            </div>
+            <Button 
+            className="submit-button"
+            onClick={submitReview}
+            >입력</Button>
+        </Container>
+    )
+}
+
+export default Reference
+*/
